e31973ddfef55294d91eb87e4893e6b79c60f9e2
oumisa <rodionovmax28@gmail.com>
2024-11-28 10:49:33 +0300

    push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import '../src/styles/style.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    const [email, setEmail] = useState('')
+    const [pass, setPass] = useState('')
+    const [emailDirty, setEmailDirty] = useState(false)
+    const [passDirty, setPassDirty] = useState(false)
+    const [emailError, setEmailError] = useState('Почта не может быть пустой!')
+    const [passError, setPassError] = useState('Пароль не может быть пустым!')
+    const [formValid, setFormValid] = useState(false)
+
+    useEffect(() => {
+        if (emailError || passError) {
+            setFormValid(false)
+        } else {
+            setFormValid(true)
+        }
+    }, [emailError, passError])
+
+    const emailHandler = (e) => {
+        setEmail(e.target.value)
+        const re =
+            /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if (!re.test(String(e.target.value))) {
+            setEmailError('Некоректная почта')
+        } else {
+            setEmailError('')
+        }
+    }
+
+    const passHandler = (e) => {
+        setPass(e.target.value)
+        if (e.target.value.length <= 5) {
+            setPassError('Пароль должен быть длиннее 5 символов')
+            if (!e.target.value) {
+                setPassError('Пароль не может быть пустым')
+            }
+        } else {
+            setPassError('')
+        }
+    }
+
+    const blurHandler = (e) => {
+        switch (e.target.name) {
+            case 'email':
+                setEmailDirty(true)
+                break
+            case 'password':
+                setPassDirty(true)
+                break
+        }
+    }
+
+    return (
+        <header className="auntification">
+            <h2 className="auntification__title">Вход в учётную запись</h2>
+            <form className="auntification__form">
+                {(emailDirty && emailError) && <div className="auntification__form_email-error" style={{color: 'red'}}>{emailError}</div>}
+                <input className="auntification__form_email" onChange={e => emailHandler(e)} value={email} onBlur={e => blurHandler(e)} name="email" type="text" placeholder="Почта"/>
+                {(passError && passDirty) && <div className="auntification__form_pass-error" style={{color: 'red'}}>{passError}</div>}
+                <input className="auntification__form_pass" onChange={e => passHandler(e)} value={pass} onBlur={e => blurHandler(e)} name="password" type="password" placeholder="Пароль"/>
+                <button className="auntification__form_button" disabled={!formValid} type="submit">Вход</button>
+            </form>
+        </header>
   );
 }
 
